006acdc69b206fa8893133fe4fe813c878038b9c
JayaniLakshika <jayanilakshika76@gmail.com>
2025-11-23 12:37:32 +1100

    Added test file

--- tests/testthat/test-read-ejson.R
+++ tests/testthat/test-read-ejson.R
@@ -1,3 +1,3 @@
 test_that("read_ejson works on missing file", {
-  expect_error(read_ejson("2010_PHC_Kiribati_EnumArea_3832.geojson"))
+  expect_error(read_ejson("data/2010_PHC_Kiribati_EnumArea_3832.geojson"))
 })
